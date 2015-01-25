a7718a38be698272b9061edeef3e92f31d8d7853
sashas777 <cybergomel@gmail.com>
2015-01-24 21:21:49 -0500

    Home Charts

--- src/com/magapp/main/HomeFragment.java
+++ src/com/magapp/main/HomeFragment.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -27,27 +28,25 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.androidplot.Plot;
-import com.androidplot.ui.AnchorPosition;
-import com.androidplot.ui.SizeLayoutType;
-import com.androidplot.ui.SizeMetrics;
-import com.androidplot.ui.XLayoutStyle;
-import com.androidplot.ui.YLayoutStyle;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.PointLabelFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
-import com.androidplot.xy.XYStepMode;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.magapp.connect.RequestInterface;
 import com.magapp.connect.RequestTask;
+
+import com.github.mikephil.charting.utils.YLabels;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
 public class HomeFragment extends Fragment  implements RequestInterface{
 
 	private View rootView;
 	private SimpleDateFormat OrdersDateFormat = new SimpleDateFormat("hha");
 	private Object[] AmountsData, OrdersData;
-	  
+
+
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.home, null);
@@ -56,11 +55,39 @@ public class HomeFragment extends Fragment  implements RequestInterface{
 		 
 		Vector params = new Vector();		 	 
 		task = new RequestTask(this, getActivity());
-		task.execute(params);				 
-		
-		// AddOrdersPlot(new Number[]{},new Number[]{});
-		// AddAmountsPlot(new Number[]{},new Number[]{});
-		 
+
+		/*new chart*/
+		LineChart chart = (LineChart) rootView.findViewById(R.id.chart);
+		chart.setStartAtZero(true);
+		chart.setDrawYValues(false);
+		chart.setDrawBorder(false);
+		chart.setDrawLegend(false);
+		chart.setScaleEnabled(false);
+		chart.setDragEnabled(false);
+		chart.setPinchZoom(false);
+		chart.setTouchEnabled(false);
+		chart.setDrawGridBackground(false);
+		chart.setDescription("Orders");
+		chart.setBackgroundColor(Color.WHITE);
+		YLabels yl = chart.getYLabels();
+		yl.setFormatter(new IntegerValueFormatter());
+
+		LineChart AmountsChart = (LineChart) rootView.findViewById(R.id.chart2);
+		AmountsChart.setStartAtZero(true);
+		AmountsChart.setDrawYValues(false);
+		AmountsChart.setDrawBorder(false);
+		AmountsChart.setDrawLegend(false);
+		AmountsChart.setScaleEnabled(false);
+		AmountsChart.setDragEnabled(false);
+		AmountsChart.setTouchEnabled(false);
+		AmountsChart.setPinchZoom(false);
+		AmountsChart.setDrawGridBackground(false);
+		AmountsChart.setDescription("Amounts");
+		AmountsChart.setBackgroundColor(Color.WHITE);
+		/*New chart*/
+
+		task.execute(params);
+
 		return rootView;
 	}
 
@@ -89,209 +116,159 @@ public class HomeFragment extends Fragment  implements RequestInterface{
 		getActivity().finish();		 
 	 }
 
+
+
 	public void AddOrdersSpinner() {
-		Spinner OrdersPlotOptions = (Spinner) rootView
+	 	Spinner OrdersPlotOptions = (Spinner) rootView
 				.findViewById(R.id.OrdersPlotOptions);
-		String[] plot_item_data = new String[] { "Last 24 Hours",
-				"Last 7 Days", "Current Month", "YTD", "2YTD" };
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
-				getActivity(), R.layout.custom_spinner_row, plot_item_data);
+		String[] plot_item_data = new String[] { "Last 24 Hours", "Last 7 Days", "Current Month", "YTD", "2YTD" };
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), R.layout.custom_spinner_row, plot_item_data);
 		dataAdapter.setDropDownViewResource(R.layout.custom_spinner_row);
 		OrdersPlotOptions.setAdapter(dataAdapter);
 
-		OrdersPlotOptions
-				.setOnItemSelectedListener(new OnItemSelectedListener() {
+		OrdersPlotOptions.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-					public void onItemSelected(AdapterView<?> arg0, View view,
-							int position, long id) {
+			public void onItemSelected(AdapterView<?> arg0, View view, int position, long id) {
 
-						if (OrdersData != null) {
-							switch (position) {
-							case 0:
-								OrdersDateFormat = new SimpleDateFormat("hha");
-								break;
-							case 1:
-								OrdersDateFormat = new SimpleDateFormat("MM-dd");
-								break;
-							case 2:
-								OrdersDateFormat = new SimpleDateFormat("MM-dd");
-								break;
-							case 3:
-								OrdersDateFormat = new SimpleDateFormat(
-										"MM-yyyy");
-								break;
-							case 4:
-								OrdersDateFormat = new SimpleDateFormat(
-										"MM-yyyy");
-								break;
-							}
-
-							PrepareChartData((HashMap) OrdersData[position],"orders");
-                            PrepareChartData((HashMap) AmountsData[position],"amounts");
-						}
-
+				if (OrdersData != null) {
+					switch (position) {
+						case 0:
+							OrdersDateFormat = new SimpleDateFormat("hha");
+							break;
+						case 1:
+							OrdersDateFormat = new SimpleDateFormat("MM-dd");
+							break;
+						case 2:
+							OrdersDateFormat = new SimpleDateFormat("MM-dd");
+							break;
+						case 3:
+							OrdersDateFormat = new SimpleDateFormat("MM-yyyy");
+							break;
+						case 4:
+							OrdersDateFormat = new SimpleDateFormat("MM-yyyy");
+							break;
 					}
+					PrepareChartData((HashMap) OrdersData[position],(HashMap)  AmountsData[position]);
+				}
 
-					public void onNothingSelected(AdapterView<?> arg0) {
-					}
-				});
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
 	}
 
-	public void PrepareChartData(HashMap chart_info_obj, String type) {
-
-		Object[] x_obj = (Object[]) chart_info_obj.get("x");
-		Object[] y_obj = (Object[]) chart_info_obj.get("y");
-		Object[] totals_obj = (Object[]) chart_info_obj.get("totals");
-
-		Number[] x_row = new Number[x_obj.length];
-		Number[] y_row = new Number[y_obj.length];
-		for (int i = 0; i < x_obj.length; i++) {
-			x_row[i] = (Number) x_obj[i];
-			y_row[i] = (Number) y_obj[i];
-		}
+	public void PrepareChartData(HashMap orders_info_obj, HashMap amounts_info_obj) {
 
 		TextView Revenue = (TextView) rootView.findViewById(R.id.OrdersRevenueValue);
 		TextView Tax = (TextView) rootView.findViewById(R.id.OrdersTaxValue);
 		TextView Shipping = (TextView) rootView.findViewById(R.id.OrdersShippingValue);
 		TextView Qty = (TextView) rootView.findViewById(R.id.OrdersQtyValue);
 
-		if (type == "orders") {
-			AddOrdersPlot(y_row, x_row);
-		} else if (type == "amounts") {
-			AddAmountsPlot(y_row, x_row);
+		LineChart chart = (LineChart) rootView.findViewById(R.id.chart);
+		LineChart AmountsChart = (LineChart) rootView.findViewById(R.id.chart2);
+
+		ArrayList<String> xVals = new ArrayList<String>();
+		ArrayList<Entry> vals1 = new ArrayList<Entry>();
+
+		Object[] x_obj = (Object[]) orders_info_obj.get("x");
+		Object[] y_obj = (Object[]) orders_info_obj.get("y");
+		Object[] totals_obj = (Object[]) orders_info_obj.get("totals");
+
+		/* Amounts */
+		Object[] amounts_x_obj = (Object[]) amounts_info_obj.get("x");
+		Object[] amounts_y_obj = (Object[]) amounts_info_obj.get("y");
+		ArrayList<String> xAmountsVals = new ArrayList<String>();
+		ArrayList<Entry> AmountsVals1 = new ArrayList<Entry>();
+		/* Amounts */
+
+		OrdersDateFormat.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
+
+		for (int i = 0; i < x_obj.length; i++) {
+			java.util.Date date = new java.util.Date(((Number) x_obj[i]).longValue() * 1000);
+			xVals.add(OrdersDateFormat.format(date) + "   ");
+			vals1.add(new Entry(  (int) new Double(y_obj[i].toString()).intValue(), i));
+		}
+
+		/* Amounts */
+		for (int i = 0; i < amounts_x_obj.length; i++) {
+			java.util.Date date = new java.util.Date(((Number) amounts_x_obj[i]).longValue() * 1000);
+			xAmountsVals.add(OrdersDateFormat.format(date) + "   ");
+			AmountsVals1.add(new Entry( Float.valueOf(amounts_y_obj[i].toString()), i));
 
 		}
+
+		LineDataSet set2 = new LineDataSet(AmountsVals1, "Amounts");
+		set2.setDrawCubic(true);
+		set2.setCubicIntensity(0.2f);
+		set2.setDrawFilled(true);
+		set2.setDrawCircles(false);
+		set2.setLineWidth(2f);
+		set2.setCircleSize(5f);
+		set2.setHighLightColor(Color.rgb(244, 117, 117));
+		set2.setColor(Color.rgb(234, 118, 1));
+		/* Amounts */
+
+		LineDataSet set1 = new LineDataSet(vals1, "Orders");
+		set1.setDrawCubic(true);
+		set1.setCubicIntensity(0.2f);
+		set1.setDrawFilled(true);
+		set1.setDrawCircles(false);
+		set1.setLineWidth(2f);
+		set1.setCircleSize(5f);
+		set1.setHighLightColor(Color.rgb(244, 117, 117));
+		set1.setColor(Color.rgb(234, 118, 1));
+
+		ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+		dataSets.add(set1);
+
+		/* Amounts */
+		ArrayList<LineDataSet> AmountsdataSets = new ArrayList<LineDataSet>();
+		AmountsdataSets.add(set2);
+		LineData Amountsdata = new LineData(xAmountsVals, set2);
+		AmountsChart.setUnit(" $");
+		AmountsChart.setData(Amountsdata);
+		AmountsChart.fitScreen();
+		AmountsChart.setScaleMinima(0, 0);
+		/* Amounts */
+
+		LineData data = new LineData(xVals, dataSets);
+
+		// set data
+		chart.setData(data);
+		chart.fitScreen();
+		chart.setScaleMinima(0, 0);
 
 		Revenue.setText(totals_obj[0].toString());
 		Tax.setText(totals_obj[1].toString());
 		Shipping.setText(totals_obj[2].toString());
 		Qty.setText(totals_obj[3].toString());
 
-	}
-
-	public void AddOrdersPlot(Number[] orders_num, Number[] order_dates) {
-
-		XYPlot OrdersPlot = (XYPlot) rootView.findViewById(R.id.OrdersPlot);
-		OrdersPlot.clear();
-
-		XYSeries series2 = new SimpleXYSeries(Arrays.asList(order_dates), Arrays.asList(orders_num), "");
-
-		OrdersPlot.setBorderStyle(Plot.BorderStyle.SQUARE, null, null);
-		OrdersPlot.setPlotMargins(0, 0, 0, 0);
-
-		SizeMetrics sm = new SizeMetrics(0, SizeLayoutType.FILL, 0, SizeLayoutType.FILL);
-		OrdersPlot.getGraphWidget().setSize(sm);
-		OrdersPlot.getGraphWidget().position(0, XLayoutStyle.RELATIVE_TO_LEFT, 0, YLayoutStyle.RELATIVE_TO_TOP, AnchorPosition.LEFT_TOP);
-
-		// OrdersPlot.setTicksPerRangeLabel(1);
-        /*test*/
-		/* OrdersPlot.setDomainStep(XYStepMode.SUBDIVIDE, 12); */
-		OrdersPlot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 1);
-		// mySimpleXYPlot.setRangeBoundaries(0, BoundaryMode.FIXED, 6,
-		// BoundaryMode.GROW);
-		// get rid of decimal points in our range labels:
-		OrdersPlot.setRangeValueFormat(new DecimalFormat("0"));
-
-		Paint lineFill = new Paint();
-		lineFill.setColor(Color.parseColor("#F4D4B2"));
-
-		LineAndPointFormatter formatter = new LineAndPointFormatter(
-				Color.parseColor("#DB4814"), Color.parseColor("#DB4814"),
-				Color.parseColor("#F4D4B2"), (PointLabelFormatter) null);
-		formatter.setFillPaint(lineFill);
-
-		OrdersPlot.addSeries(series2, formatter);
-
-		OrdersPlot.setDomainValueFormat(new Format() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public StringBuffer format(Object obj, StringBuffer toAppendTo,
-					FieldPosition pos) {
-				java.util.Date date = new java.util.Date(((Number) obj).longValue()*1000);
-
-
-                OrdersDateFormat.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
-
-				return OrdersDateFormat.format(date, toAppendTo, pos);
-			}
-
-			@Override
-			public Object parseObject(String source, ParsePosition pos) {
-				return null;
-			}
-		});
-
-		// redraw
-		OrdersPlot.redraw();
-	}
-
-	public void AddAmountsPlot(Number[] amounts_num, Number[] amounts_dates) {
-
-		XYPlot AmountsPlot = (XYPlot) rootView.findViewById(R.id.AmountsPlot);
-		AmountsPlot.clear();
-
-		XYSeries series2 = new SimpleXYSeries(Arrays.asList(amounts_dates),
-				Arrays.asList(amounts_num), "");
-
-		AmountsPlot.setBorderStyle(Plot.BorderStyle.SQUARE, null, null);
-		AmountsPlot.setPlotMargins(0, 0, 0, 0);
-
-		SizeMetrics sm = new SizeMetrics(0, SizeLayoutType.FILL, 0,
-				SizeLayoutType.FILL);
-		AmountsPlot.getGraphWidget().setSize(sm);
-		AmountsPlot.getGraphWidget().position(0, XLayoutStyle.RELATIVE_TO_LEFT,
-				0, YLayoutStyle.RELATIVE_TO_TOP, AnchorPosition.LEFT_TOP);
-
-		// mySimpleXYPlot.setTicksPerRangeLabel(1);
-		// mySimpleXYPlot.setDomainStep(XYStepMode.SUBDIVIDE, points);
-		// AmountsPlot.setRangeStep(XYStepMode.INCREMENT_BY_VAL,1);
-		// mySimpleXYPlot.setRangeBoundaries(0, BoundaryMode.FIXED, 6,
-		// BoundaryMode.GROW);
-		// get rid of decimal points in our range labels:
-		AmountsPlot.setRangeValueFormat(new DecimalFormat("0"));
-
-		Paint lineFill = new Paint();
-		lineFill.setColor(Color.parseColor("#F4D4B2"));
-
-		LineAndPointFormatter formatter = new LineAndPointFormatter(
-				Color.parseColor("#DB4814"), Color.parseColor("#DB4814"),
-				Color.parseColor("#F4D4B2"), (PointLabelFormatter) null);
-		formatter.setFillPaint(lineFill);
-		AmountsPlot.addSeries(series2, formatter);
-
-		AmountsPlot.setDomainValueFormat(new Format() {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public StringBuffer format(Object obj, StringBuffer toAppendTo,
-					FieldPosition pos) {
-				java.util.Date date = new java.util.Date(((Number) obj)
-						.longValue() * 1000);
-                OrdersDateFormat.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
-				return OrdersDateFormat.format(date, toAppendTo, pos);
-			}
-
-			@Override
-			public Object parseObject(String source, ParsePosition pos) {
-				return null;
-			}
-		});
-
-		// redraw
-		AmountsPlot.redraw();
+		chart.animateXY(2000, 2000);
+		AmountsChart.animateXY(2000, 2000);
+		chart.invalidate();
+		AmountsChart.invalidate();
 	}
 
 	public void SetChartData(Object data) {
-
 		HashMap data_map = (HashMap) data;
- 
-		OrdersData = (Object[]) data_map.get("orders");
-		AddOrdersSpinner();			 
- 
-		AmountsData = (Object[]) data_map.get("amounts");
 
+		OrdersData = (Object[]) data_map.get("orders");
+		AddOrdersSpinner();
+		AmountsData = (Object[]) data_map.get("amounts");
+	}
+	public class IntegerValueFormatter implements ValueFormatter {
+		@Override
+		public String getFormattedValue(float value) {
+			Integer val=(int) value;
+
+			if (val==0 && value>0) {
+				value=value*10;
+				val=(int) value;
+			}
+
+			return val.toString();
+		}
 	}
 
- 
 }
